60f0ddf076bb3fd475b3e4f6cf91af7450b28356
Oru <tejirioru@gmail.com>
2024-04-29 16:45:37 +0100

    minor changes to the edit page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          The World is yours
         </a>
       </header>
     </div>
