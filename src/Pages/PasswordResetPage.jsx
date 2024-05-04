084a4ceabcf17cbeede13689e93ace8bcd00dd15
Distrustfultie <efecherebor22@gmail.com>
2024-05-04 10:56:41 +0100

    Reset3

--- src/Pages/PasswordResetPage.jsx
+++ src/Pages/PasswordResetPage.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 // import Header from '../Components/Header';
 // import Footer from '../Components/Footer';
 import PasswordResetForm from '../Components/PasswordResetForm';
+import PadLock from '../../src/uil_padlock.png';
 
 const PasswordResetPage = () => {
   return (
     <div>
       {/* <Header /> */}
       <div className="container">
-        <h2>Password Reset</h2>
+      <img src={PadLock} alt='Lock' />
+        <h1>Forgot password</h1>
+        <p>Please enter your mail to reset password</p>
         <PasswordResetForm />
       </div>
       {/* <Footer /> */}
